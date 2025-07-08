cc0dd48fdd6338e1f88de767d615f7e9f2faaf8b
pasindu526 <pasindumadusanka526@gmail.com>
2025-07-08 08:48:34 +0530

    Modify Marketing page - v3

--- src/component/Marketing/RecentWork.tsx
+++ src/component/Marketing/RecentWork.tsx
@@ -6,7 +6,7 @@ const RecentWork: React.FC = () => {
         <h1 className="text-[26px] md:text-3xl lg:text-5xl lg:leading-[62px] font-bold text-center">
           What We've Done
         </h1>
-        <p className="lg:w-1/2 mt-4 text-sm lg:text-lg leading-6 md:leading-7 lg:leading-8 font-light text-center">
+        <p className="md:w-3/4 lg:w-1/2 mt-4 text-sm lg:text-lg leading-6 md:leading-7 lg:leading-8 font-light text-center">
           Explore our recent projects to see how strategy, creativity, and
           performance come together to drive meaningful results for our clients.
         </p>
--- src/pages/Merketing.tsx
+++ src/pages/Merketing.tsx
@@ -3,38 +3,50 @@ import Hero from "../component/Marketing/Hero";
 import Process from "../component/Marketing/Process";
 import RecentWork from "../component/Marketing/RecentWork";
 import Service from "../component/Marketing/Service";
+import nav from "../assets/marketing/Navbar.png";
+import footer from "../assets/marketing/Footer.png";
 
 const Merketing: React.FC = () => {
   return (
-    <div className="p-3.5 text-text">
-      {/* Header */}
-      <header className="bg-[#01213A] mb-3.5 h-[67px] rounded-[20px]"></header>
+    <>
+      <div className="m-3.5 text-text">
+        {/* Header */}
+        <header className="bg-[#01213A] mb-3.5 h-[67px] rounded-[20px]">
+          <img src={nav} />
+        </header>
 
-      {/* Hero*/}
-      <section className="w-11/12 mx-auto lg:pt-14">
-        <Hero />
-      </section>
+        {/* Hero*/}
+        <section className="w-full lg:pt-14 rounded-[20px]">
+          <Hero />
+        </section>
 
-      {/* Services */}
-      <section className="mt-20 md:mt-30 bg-white rounded-[20px]">
-        <Service />
-      </section>
+        {/* Services */}
+        <section
+          className="mt-20 md:mt-30 bg-white rounded-[20px]"
+          id="services"
+        >
+          <Service />
+        </section>
 
-      {/* Process */}
-      <section className="mt-20 md:mt-30 bg-gradient-to-r from-[#01213A] from-5% to-[#035BA0] to-100% rounded-[20px]">
-        <Process />
-      </section>
+        {/* Process */}
+        <section className="mt-20 md:mt-30 bg-gradient-to-r from-[#01213A] from-5% to-[#035BA0] to-100% rounded-[20px]">
+          <Process />
+        </section>
 
-      {/* RecentWork */}
-      <section className="mt-20 md:mt-30 bg-transparent rounded-[20px]">
-        <RecentWork />
-      </section>
+        {/* RecentWork */}
+        <section className="mt-20 md:mt-30 bg-transparent rounded-[20px]">
+          <RecentWork />
+        </section>
 
-      {/* Contact */}
-      <section className="mt-20 md:mt-30 bg-white rounded-[20px]">
-        <Contact />
-      </section>
-    </div>
+        {/* Contact */}
+        <section className="mt-20 md:mt-30 bg-white rounded-[20px]">
+          <Contact />
+        </section>
+      </div>
+      <footer className="mt-20 lg:mt-30">
+        <img src={footer} />
+      </footer>
+    </>
   );
 };
 
