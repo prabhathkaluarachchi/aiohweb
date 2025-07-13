e07597e5b189f663cda73e04e8239d43a094ba44
pasindu526 <pasindumadusanka526@gmail.com>
2025-07-13 15:33:40 +0530

    Modify Service section - v2

--- src/component/Marketing/Service.tsx
+++ src/component/Marketing/Service.tsx
@@ -1,11 +1,16 @@
 import { GoArrowUpRight } from "react-icons/go";
+
+// bg images
 import sc1 from "../../assets/marketing/sc1.png";
 import sc2 from "../../assets/marketing/sc2.png";
 import sc3 from "../../assets/marketing/sc3.png";
 import sc4 from "../../assets/marketing/sc4.png";
 import sc5 from "../../assets/marketing/sc5.png";
 import sc6 from "../../assets/marketing/sc6.png";
+
 import { motion } from "framer-motion";
+
+import { useNavigate } from "react-router-dom";
 
 const services = [
   {
@@ -46,23 +51,48 @@ const services = [
   },
 ];
 
+const slugify = (text: string) =>
+  text
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "");
+
 const Service: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="w-5/6 mx-auto py-4">
+    <div className="w-11/12 md:w-5/6 mx-auto py-4">
       {/* sec heading */}
-      <div className="flex flex-col items-center">
-        <h1 className="text-[26px] md:text-3xl lg:text-5xl lg:leading-[62px] font-bold text-center">
+      <motion.div
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        viewport={{ once: true }}
+        className="flex flex-col items-center"
+      >
+        <motion.h1
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          className="md:w-full w-3/4 text-[26px] md:text-3xl lg:text-5xl lg:leading-[62px] font-bold text-center"
+        >
           Expertise That Powers Growth
-        </h1>
-        <p className="md:w-5/6 lg:w-2/3 mt-4 text-sm lg:text-lg leading-6 md:leading-7 lg:leading-8 font-light text-center">
-          We deliver data-driven marketing services tailored to your brand —
+        </motion.h1>
+        <p className="md:w-5/6 lg:w-3/4 mt-4 text-sm lg:text-lg leading-6 md:leading-7 lg:leading-8 font-light text-center">
+          We deliver data-driven marketing services tailored to your brand
           combining strategy, creativity, and technology to accelerate your
           digital growth.
         </p>
-      </div>
+      </motion.div>
 
       {/* servise card sec */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 w-5/6 mx-auto py-6">
+      <motion.div
+        initial={{ opacity: 0, x: 20 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, delay: 0.4 }}
+        viewport={{ once: true }}
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 w-5/6 mx-auto py-10"
+      >
         {services.map((service, index) => (
           <div
             key={index}
@@ -72,10 +102,10 @@ const Service: React.FC = () => {
             <img
               src={service.img}
               alt={service.title}
-              className="object-cover w-full h-40 md:h-auto duration-500 group-hover:scale-105 transition "
+              className="object-cover w-full h-56 md:h-auto duration-500 group-hover:scale-105 transition"
             />
 
-            {/* Floating Label with Line */}
+            {/* Floating Label with Outline */}
             <div className="absolute top-4 left-4 right-4 items-center z-50 mx-auto">
               <span className="absolute -top-3.5 md:-top-2.5 lg:-top-3  left-4 mr-4 rounded-xl bg-white/0 backdrop-blur-lg px-1 text-slate-400 text-sm md:text-md lg:text-lg font-medium text-white">
                 {service.title}
@@ -83,29 +113,54 @@ const Service: React.FC = () => {
             </div>
             <div className="absolute inset-0 rounded-xl border-3 border-white z-40 pointer-events-none m-3 md:m-4 lg:m-4.5"></div>
 
-            <div className="absolute bottom-5 right-5 md:bottom-7 md:right-7 bg-primary text-secondary w-8 h-8 md:w-10 md:h-10 flex items-center justify-center rounded-full z-10 group-hover:scale-110 transition cursor-pointer">
-              <GoArrowUpRight className="text-base md:text-lg" />
+            {/* explore button -desktop/tab */}
+            <div className="group-hover:hidden hidden lg:block">
+              <button className="absolute bottom-5 right-5 md:bottom-7 md:right-7 bg-primary text-secondary w-8 h-8 md:w-10 md:h-10 flex items-center justify-center rounded-full z-10 group-hover:scale-110 transition cursor-pointer">
+                <GoArrowUpRight className="text-base md:text-lg" />
+              </button>
             </div>
 
             {/* hover effect */}
-            <div className="hidden group-hover:block">
+            <div className="block lg:hidden group-hover:block">
               <motion.div
                 initial={{ opacity: 0, y: 10 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.4, delay: 0 }}
-                className="absolute min-h-[50%] py-2 bottom-0 left-0 w-full bg-[#01213A]/20 backdrop-blur-md flex-col items-center justify-between z-30 px-8"
+                className="absolute h-[50%] py-2 bottom-0 left-0 w-full bg-[#01213A]/20 backdrop-blur-md flex-col items-center justify-between z-30 px-8"
               >
-                <p className="text-justify leading-[22px] text-sm text-white">
+                <p className="text-justify leading-[22px] text-xs lg:text-sm text-white">
                   {service.description}
                 </p>
-                <button className="w-full lg:w-fit lg:my-4 px-4 py-2 bg-primary hover:bg-primary/80 text-xs rounded-full cursor-pointer font-medium">
+
+                {/* explore button - in hover sec */}
+                <button
+                  key={index}
+                  onClick={() =>
+                    navigate(`/services/${slugify(service.title)}`, {
+                      state: service,
+                    })
+                  }
+                  className="hidden md:block w-full md:w-fit md:my-2 lg:my-4 px-4 py-2 bg-primary hover:bg-primary/80 text-xs rounded-full cursor-pointer font-medium"
+                >
                   Explore Service
+                </button>
+
+                {/* explore button - mobile */}
+                <button
+                  onClick={() =>
+                    navigate(`/services/${slugify(service.title)}`, {
+                      state: service,
+                    })
+                  }
+                  className=" md:hidden absolute bottom-5 right-7 md:bottom-7 md:right-7 bg-primary text-secondary w-8 h-8 md:w-10 md:h-10 flex items-center justify-center rounded-full z-10 group-hover:scale-110 transition cursor-pointer"
+                >
+                  <GoArrowUpRight className="text-base md:text-lg" />
                 </button>
               </motion.div>
             </div>
           </div>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
